ecbd8de35f5b68ee79bf03a8ef54159798488a50
Yaroslav Zharov <mart.slaaf@gmail.com>
2019-10-11 12:03:24 +0200

    [+] Added git configuration

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
-# install vim
 $PWD = pwd
+
+# configure vim
 ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# tmux config
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+
+# config git
+ln -s $PWD/.gitignore $HOME/.gitignore
+git config --global core.editor "vim"
+git config --global core.excludesfile ~/.gitignore
+git config --global user.name "Yaroslav Zharov"
+git config --global user.email "mart.slaaf@gmail.com"
